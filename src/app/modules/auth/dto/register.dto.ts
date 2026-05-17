7d2feddce03683a5274d36ba68684340f5898939
HeyVenom49 <durgeshpandeysmart@gmail.com>
2026-05-18 03:22:22 +0530

    add RegisterDto for user registration with validation schema

---
+++ src/app/modules/auth/dto/register.dto.ts
@@ -0,0 +1,11 @@
+import { type } from "arktype";
+import { BaseDto } from "../../../common/dto/base.dto.ts";
+
+export class RegisterDto extends BaseDto {
+  static schema = type({
+    username: "string > 3",
+    email: "string.email",
+    password: "string >= 6",
+    role: "'customer'",
+  }).onUndeclaredKey("reject");
+}
