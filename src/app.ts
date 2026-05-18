ad9bd2ce07e59c6ffab455a4b6dfc7bc430a823b
HeyVenom49 <durgeshpandeysmart@gmail.com>
2026-05-18 15:38:41 +0530

    add express middleware for JSON parsing, URL encoding, and cookie parsing; set up authentication routes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
+import cookieParser from "cookie-parser";
 import express, { type Express } from "express";
+import authRoute from "./app/modules/auth/auth.routes.ts";
 
 const app: Express = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+app.use("/api/auth/", authRoute);
 
 export default app;
